692d2185d904ad10b2d10532f3c9cc97bbdee1fa
egor-kuch <egor-kuch@yandex-team.ru>
2022-07-13 17:37:10 +0300

    Логирование запросов

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -3,6 +3,18 @@ const rateLimiter = require("express-rate-limit");
 const slowDown = require("express-slow-down");
 const disablePoweredBy = require("./disablePoweredBy");
 const errorHandler = require("./errorHandler");
+const logger = require('./logger');
+
+const limiter = rateLimiter({
+  windowMs: 1 * 60 * 1000,
+  max: 120,
+});
+
+const speedLimiter = slowDown({
+  windowMs: 1 * 60 * 1000,
+  delayAfter: 100,
+  delayMs: 1000,
+});
 
 module.exports = (app) => {
   app.use(json());
@@ -11,16 +23,7 @@ module.exports = (app) => {
 
   app.use(errorHandler);
 
-  const limiter = rateLimiter({
-    windowMs: 1 * 60 * 1000,
-    max: 120,
-  });
-
-  const speedLimiter = slowDown({
-    windowMs: 1 * 60 * 1000,
-    delayAfter: 100,
-    delayMs: 1000,
-  });
+  app.use(logger);
 
   app.use(speedLimiter);
   app.use(limiter);
