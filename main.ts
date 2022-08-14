6bc53bd06b3b6ec45ca16ff655291e4a525f5f68
Pavel <Pikcelll@yandex-team.ru>
2022-08-14 11:45:18 +0300

    УХеа

---
+++ main.ts
@@ -0,0 +1,9 @@
+// interface f {
+//   type: string;
+//   color: string;
+//   level: string; //15A
+//   network: string;
+//   values: Array<string | number>;
+//   offpeak: boolean;
+//   KO: boolean; // выбрано KO - true, иначе false
+// }
---
+++ src/modules/filter/filter.js
@@ -0,0 +1,10 @@
+if (network === "PS.eu") {
+  if (MELE(1, 7.5, 21321, 23132) && MELE(1, 9)) return true;
+  if (MELE(109, 150000)) return true;
+  if (MELEME(121, 150, 250000)) return true;
+}
+if (network === "GG") {
+  if (MELE(1, 9) && MELE(1, 7.5)) return true;
+  if (MELE(109, 150000)) return true;
+  if (MELEME(121, 150, 250000)) return true;
+}
