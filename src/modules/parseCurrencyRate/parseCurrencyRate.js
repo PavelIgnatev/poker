c5c2eeaff0ee6ad5704ce98058a82dba62868fe2
Ruslan Rasulov <ruxxxa10@gmail.com>
2022-10-28 17:43:07 +0300

    update currency rate function logic

--- src/modules/parseCurrencyRate/parseCurrencyRate.js
+++ src/modules/parseCurrencyRate/parseCurrencyRate.js
@@ -1,5 +1,10 @@
 const axios = require("axios");
 const { username_api_key, password_api_key } = require("../../constants");
+const fs = require("fs");
+
+const writeCurrency = (data) => {
+  fs.writeFile("../../store/currency/currency.json", { data: data });
+};
 
 const parseFirstAPI = async (to, from) => {
   const url = "https://xecdapi.xe.com/v1/convert_to.json/";
@@ -15,8 +20,7 @@ const parseFirstAPI = async (to, from) => {
         password: password_api_key,
       },
     })
-    .then((res) => res.data.from[0].mid)
-    .catch((rej) => rej.response);
+    .then((res) => res.data.from[0].mid);
 };
 
 const parseSecondAPI = async (currency) => {
@@ -24,20 +28,26 @@ const parseSecondAPI = async (currency) => {
   return axios
     .get(url)
     .then((res) => res.data.rates[currency])
-    .catch((rej) => rej.response);
+    .then((res) => (res ? res : Promise.reject()));
 };
 
 async function parseCurrencyRate() {
-  const firstReq = await parseFirstAPI("USD", "CNY");
-  const secondReq = await parseSecondAPI("CNY");
+  return parseFirstAPI("USD", "CNY")
+    .catch(() => {
+      console.log("Первое API сломано");
 
-  if (firstReq) {
-    return firstReq;
-  } else if (secondReq) {
-    return secondReq;
-  } else {
-    return "ERROR";
-  }
+      return parseSecondAPI("CNY");
+    })
+    .catch(() => {
+      console.log("Второе API сломано");
+    })
+    .then((res) => {
+      writeCurrency(res);
+      return res;
+    });
 }
+(async () => {
+  console.log(await parseCurrencyRate());
+})();
 
 module.exports = { parseCurrencyRate };
