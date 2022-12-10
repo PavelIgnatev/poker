cb0e311b629b085b462464cbd0d0048e41646d92
Pavel Ignatev <palllkaignatev@gmail.com>
2022-12-10 14:20:56 +0500

    хей хей хей

--- src/modules/update/updateServer.js
+++ src/modules/update/updateServer.js
@@ -19,7 +19,7 @@ const updateServer = async () => {
   // Отправка писем
   try {
     console.log("Начинаю отправлять письма");
-    // await collectionStatistics();
+    await collectionStatistics();
   } catch (error) {
     console.log("Ошибка при отправке писем: ", error);
   }
