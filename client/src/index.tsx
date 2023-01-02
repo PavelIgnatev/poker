2da6b68adf06e82554ca94678bff99e696c3036d
Pavel Ignatev <palllkaignatev@gmail.com>
2023-01-02 16:42:41 +0500

    дефолтная таймзона клиента - utc

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,4 +3,6 @@ import { Pages } from "./pages/index";
 import "./assets/style/index.scss";
 import "./store/init";
 
+process.env.TZ = "UTC";
+
 ReactDOM.render(<Pages />, document.getElementById("root"));
