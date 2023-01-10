f4c8539de8ca6fc5b1b12e1130faa50908aa00e7
Pavel Ignatev <palllkaignatev@gmail.com>
2023-01-10 23:16:50 +0500

    сменил порт и пароль админа

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 module.exports = {
   networks: ["PS.eu", "IP", "Chico", "PS.es", "Party", "GG", "888", "WNMX", "WPN"],
-  adminPassword: "",
+  adminPassword: "6c630o3gUmIXxl2VUExd2GgjDmhC9WTs",
   configPath: "src/store/config/config.json",
   samplePath: "src/store/sample/sample.json",
   offpeakPath: "src/store/offpeak/offpeak.json",
