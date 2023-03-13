dab13d561120d818d7d09ebeb470dc4bd5509873
Pavel Ignatev <palllkaignatev@gmail.com>
2023-03-13 21:38:53 +0400

    Сделал фиксы все

--- src/modules/crons/crons.js
+++ src/modules/crons/crons.js
@@ -4,18 +4,28 @@ const { parseCurrencyRate } = require("../currencyRate/parseCurrencyRate");
 const { updateServer } = require("../update/updateServer");
 const { writeFile } = require("../../utils/promisify");
 
+const startValue = 81;
+const arrayLength = 9;
+
+function numberInArray(num, arr) {
+  return Array(arrayLength)
+    .fill()
+    .map((_, index) => startValue + index)
+    .includes(num);
+}
+
 const crons = () => {
   // при инициализации сервера чтобы isUpdated переходило в false
   writeFile("src/store/update/update.json", JSON.stringify({ isUpdated: false }));
 
   const updateServerCron = new CronJob("0 0 * * *", function () {
-    if (global.app.server.address().port === 81) {
+    if (numberInArray(global.app.server.address().port)) {
       updateServer();
     }
   });
 
   const updateCNYCourseCron = new CronJob("0 */30 * * * *", function () {
-    if (global.app.server.address().port === 81) {
+    if (numberInArray(global.app.server.address().port)) {
       parseCurrencyRate();
     }
   });
