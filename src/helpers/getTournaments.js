9fdcb5943e7b62d491b042c3a7aebbd2b6bff734
Pavel Ignatev <palllkaignatev@gmail.com>
2023-03-23 21:28:48 +0400

    store

--- src/helpers/getTournaments.js
+++ src/helpers/getTournaments.js
@@ -11,17 +11,14 @@ const getTournaments = () => {
   const filtredState = {};
 
   // Получаем 90 последних дней
-  const state = days
-    .sort((a, b) => new Date(b).getTime() - new Date(a).getTime())
-    .splice(0, Math.min(days.length, 90))
-    .reduce((accumulator, day) => {
-      const currentDay = readFileSync(`src/store/days/${day}.json`, {
-        encoding: "utf8",
-        flag: "r",
-      });
+  const state = days.reduce((accumulator, day) => {
+    const currentDay = readFileSync(`src/store/days/${day}.json`, {
+      encoding: "utf8",
+      flag: "r",
+    });
 
-      return { ...accumulator, [day]: decompress(JSON.parse(currentDay)) };
-    }, {});
+    return { ...accumulator, [day]: decompress(JSON.parse(currentDay)) };
+  }, {});
 
   Object.keys(state).forEach((day) => {
     if (!filtredState[day]) filtredState[day] = {};
